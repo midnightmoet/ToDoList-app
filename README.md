5951d97e29bb80fadf484580e061cf74b67aa081
[Lisa Miner] <midnightmoet@gmail.com>
2021-09-19 15:50:12 -0400

    final code

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1>To-Do List</h1>
+
+<p>
+<ol>
+   <li> Started this To-Do List by creating a react app: Type this in the terminal: <i>npx create-react-app my-app</i></li>
+   <li> Replace my-app with the name of your app (ie todolist-app).</li>
+</ol>
+</p>
+
+<p>
+    Have fun with this!  Feel free to suggest changes or modifications.  
+</p>
---
+++ src/components/TodoForm.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+  const inputReference = useRef(null);
+
+  useEffect(() => {
+    inputReference.current.focus();
+  });
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+    setInput('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='todo-form'>
+      {props.edit ? (
+        <>
+          <input
+            placeholder='Update your item'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputReference}
+            className='todo-input edit'
+          />
+          <button onClick={handleSubmit} className='todo-button edit'>
+            Update
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder='Add to this'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputReference}
+          />
+          <button onClick={handleSubmit} className='todo-button'>
+            Add to this list
+          </button>
+        </>
+      )}
+    </form>
+  );
+}
+
+export default TodoForm;
